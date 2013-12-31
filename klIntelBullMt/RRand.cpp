4224169e48309ff882c248224f2f35b533336b8b
Bruce Campbell LT <wavescholar@gmail.com>
2013-12-31 11:23:46 -0500

    123113

---
+++ klIntelBullMt/RRand.cpp
@@ -0,0 +1,14 @@
+// RRand.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+//void testRRand();
+int testLibRand();
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int ans =testLibRand();
+	//testRRand();
+	return 0;
+}
+
